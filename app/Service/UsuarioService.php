1335d10cd722291176e6967b18066a27db240f16
luiz1004 <luizpilan20@hotmail.com>
2024-08-23 15:03:32 -0300

    Commit inicial

---
+++ app/Service/UsuarioService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\Usuario;
+
+class UsuarioService
+{
+public function create (array $dados){
+    $user = Usuario::create([
+        'nome' => $dados ['nome'],
+         'email'=>$dados ['email'],
+          'password'=>$dados['password']
+    ]);
+   
+}
+
+public function update (){}
+
+public function delete(){}
+
+public function findById(){}
+
+Public function getAll(){}
+
+public function searchByName(){}
+
+}
