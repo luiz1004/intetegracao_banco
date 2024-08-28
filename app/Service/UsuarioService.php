c2d777627b2d7f53b922a26eecb181763e2bc438
luiz1004 <luizpilan20@hotmail.com>
2024-08-28 15:24:53 -0300

    aula dia 28-08

--- app/Service/UsuarioService.php
+++ app/Service/UsuarioService.php
@@ -3,26 +3,120 @@
 namespace App\Service;
 
 use App\Models\Usuario;
+use PHPUnit\Framework\Constraint\IsEmpty;
 
 class UsuarioService
 {
-public function create (array $dados){
-    $user = Usuario::create([
-        'nome' => $dados ['nome'],
-         'email'=>$dados ['email'],
-          'password'=>$dados['password']
-    ]);
-   
-}
+    public function create(array $dados)
+    {
+        $user = Usuario::create([
+            'nome' => $dados['nome'],
+            'email' => $dados['email'],
+            'password' => $dados['password']
+        ]);
+    }
 
-public function update (){}
+    public function update(array $dados){
+        $usuario = Usuario::find($dados ['id']);
+        if ($usuario == null){
+            return [
+                'status'=>false,
+                'message'=>'usuario nao encontrado'
+            ];
+        }
+        if(isset($dados['password'])){
+            $usuario->password = $dados['password'];
+        }
+        
+        if(isset($dados['nome'])){
+            $usuario->nome = $dados['nome'];
+        }
+       
+        if(isset($dados['email'])){
+            $usuario->email = $dados['email'];
+        }
+        
 
-public function delete(){}
+        $usuario->save();
+    }
 
-public function findById(){}
+    public function delete($id)
+    {
+        $usuarios = Usuario::find($id);
+        if ($usuarios == null) {
+            return [
+                'status' => false,
+                'message' => 'Usuario nao encontrado'
+            ];
+        }
+            $usuarios->delete();
 
-Public function getAll(){}
+            return 
+            [
+                'status' => true,
+                'message' => 'Excluido Com Sucesso'
+            ];
+        
+    }
 
-public function searchByName(){}
+    public function findById($id)
+    {
+        $usuario = Usuario::find($id);
 
+        if ($usuario == null) {
+            return [
+                'status' => false,
+                'message' => 'Usuario não encontrado'
+            ];
+        };
+
+        return [
+            'status' => 'true',
+            'message' => 'Usuario Encontrado',
+            'data' => $usuario
+        ];
+    }
+
+    public function getAll()
+    {
+        $usuarios = Usuario::all();
+        return [
+            'status' => 'True',
+            'message' => 'Pesquisa Efetuada Com Sucesso',
+            'date' => $usuarios
+        ];
+    }
+
+    public function searchByName($nome)
+    {
+        $usuarios = Usuario::where('nome', 'like', '%' . $nome . '%')->get();
+        if ($usuarios->isEmpty()) {
+            return [
+                'status' => false,
+                'message' => 'Sem Resultados'
+            ];
+        };
+        return [
+            'status' => true,
+            'message' => 'Resultados Encontrados',
+            'data' => $usuarios
+        ];
+    }
+
+    public function searchByEmail($email)
+    {
+        $usuarios = Usuario::where('email', 'like', '%' . $email . '%')->get();
+
+        if ($usuarios->isEmpty()) {
+            return [
+                'status' => false,
+                'message' => 'Sem Resultados'
+            ];
+        }
+        return [
+            'status' => true,
+            'message' => 'Resultado Encontrados',
+            'data' => $usuarios
+        ];
+    }
 }
